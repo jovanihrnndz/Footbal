d55d4d31a91bd9375701a8aeded9a1356a4d52fe
jovanihrnndz <jovanihrnndz@outlook.com>
2025-01-10 22:45:35 -0800

    Initial Commit

---
+++ Footbal/FootbalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootbalApp.swift
+//  Footbal
+//
+//  Created by jovanihrnndz on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FootbalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
